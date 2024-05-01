93b16117a16188ffa6a3d8ccac4fcf8563d80b6c
unknown <you@example.com>
2024-05-01 07:48:35 +0530

    second commit

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ dotenv.config();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
