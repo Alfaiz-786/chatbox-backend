956a429b6f119aafd64b1c28a97f82c5c490df6c
unknown <you@example.com>
2024-05-01 08:53:43 +0530

    eight commit

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["chatbox-orpin.vercel.app"],
+    origin: ["https://chatbox-orpin.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
