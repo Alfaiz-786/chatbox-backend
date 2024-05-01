0c68307a35cbdd6bfd8eb85a756f15bfd47dc296
unknown <you@example.com>
2024-05-01 08:33:06 +0530

    sixth commit

--- socket/socket.js
+++ socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: ["https://chatbox-mkfajd6h8-alfaizs-projects.vercel.app"],
     methods: ["GET", "POST"],
   },
 });
